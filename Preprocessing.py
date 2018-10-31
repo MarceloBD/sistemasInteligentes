54da8b1e2d5fa8374fe26108f0e74b8d68198322
MarceloBD <marcelodianib@gmail.com>
2018-10-31 09:09:54 -0300

    Rearranged methods and renamed variables

--- Preprocessing.py
+++ Preprocessing.py
@@ -8,30 +8,41 @@ class Preprocessing():
 	def __init__(self):
 		return
 
+	def convert_all_files(self):
+		filenames = glob.glob('files/*.txt')
+		[self.execute(filename) for filename in filenames]  
+
 	def execute(self, filename):
-		file = codecs.open(filename, 'r', 'cp1251')
-		#print(filename)
-		try:
-			u = file.read()   # now the contents have been transformed to a Unicode string
-		except:
-			file = codecs.open(filename, 'r', 'koi8-r')
-			u = file.read() 
-		newFilename = cp.deepcopy(filename)
-		newFilename = newFilename.split('/')[1]
-		newFilename = 'processedFiles/' + newFilename
-		print(newFilename)
-		out = codecs.open(newFilename, 'w', 'utf-8')
-		out.write(u)   # and now the contents have been output as UTF-8
+		newFilename = self.decode2utf8(filename)
 
 		with open(newFilename) as file:
 		    words = [word for line in file for word in line.split()]
-		    words = self.remove_final_ponctuation(words)
-		    words = list(filter(None, words)) 
-		    words = self.delete_lower_than(words, 4)
+		    words = self.apply_filters(words)
+		    
 		    out = codecs.open(newFilename, 'w', 'utf-8')
 		    out.write('\n'.join(words))
+	
+	def decode2utf8(self, filename):
+		file = codecs.open(filename, 'r', 'cp1251')
+		try:
+			raw = file.read() 
+		except:
+			file = codecs.open(filename, 'r', 'koi8-r')
+			raw = file.read() 
+		newFilename = cp.deepcopy(filename)
+		newFilename = newFilename.split('/')[1]
+		newFilename = 'processedFiles/' + newFilename
+		outFile = codecs.open(newFilename, 'w', 'utf-8')
+		outFile.write(raw) 
+		return newFilename
 
-	def remove_final_ponctuation(self, words):
+	def apply_filters(self, words):
+		words = self.remove_terminal_ponctuation(words)
+		words = list(filter(None, words)) 
+		words = self.delete_lower_than(words, 4)
+		return words
+
+	def remove_terminal_ponctuation(self, words):
 		words = [word.rstrip(string.punctuation) for word in words]
 		words = [word[::-1].rstrip(string.punctuation) for word in words]
 		return [word[::-1] for word in words]
@@ -44,12 +55,3 @@ class Preprocessing():
 		for i in range(len(remove)):
 				words.remove(remove[i])
 		return words
-
-	def read(self, file_name):
-		file = codecs.open(file_name, 'r', 'utf-8')
-		data = file.read()
-		print(data)
-
-	def convert_all_files(self):
-		filenames = glob.glob('files/*.txt')
-		[self.execute(filename) for filename in filenames]  
--- main.py
+++ main.py
@@ -3,4 +3,3 @@ from Preprocessing import Preprocessing
 if __name__ == '__main__':
 	prep = Preprocessing()
 	prep.convert_all_files()
-	#prep.read('filename')
