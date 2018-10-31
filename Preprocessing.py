4ca83b416699724579420d00fb95a15ed6ab4b74
MarceloBD <marcelodianib@gmail.com>
2018-10-31 09:32:03 -0300

    Bag of words done

--- Preprocessing.py
+++ Preprocessing.py
@@ -13,15 +13,17 @@ class Preprocessing():
 		[self.execute(filename) for filename in filenames]  
 
 	def execute(self, filename):
-		newFilename = self.decode2utf8(filename)
+		new_filename = self.decode2utf8(filename)
 
-		with open(newFilename) as file:
+		with open(new_filename) as file:
 		    words = [word for line in file for word in line.split()]
 		    words = self.apply_filters(words)
 		    
-		    out = codecs.open(newFilename, 'w', 'utf-8')
+		    out = codecs.open(new_filename, 'w', 'utf-8')
 		    out.write('\n'.join(words))
-	
+		    if(new_filename == 'processedFiles/CBR-837Aam274-288.txt'):
+		    	self.make_bag_of_words(words)
+
 	def decode2utf8(self, filename):
 		file = codecs.open(filename, 'r', 'cp1251')
 		try:
@@ -29,12 +31,12 @@ class Preprocessing():
 		except:
 			file = codecs.open(filename, 'r', 'koi8-r')
 			raw = file.read() 
-		newFilename = cp.deepcopy(filename)
-		newFilename = newFilename.split('/')[1]
-		newFilename = 'processedFiles/' + newFilename
-		outFile = codecs.open(newFilename, 'w', 'utf-8')
+		new_filename = cp.deepcopy(filename)
+		new_filename = new_filename.split('/')[1]
+		new_filename = 'processedFiles/' + new_filename
+		outFile = codecs.open(new_filename, 'w', 'utf-8')
 		outFile.write(raw) 
-		return newFilename
+		return new_filename
 
 	def apply_filters(self, words):
 		words = self.remove_terminal_ponctuation(words)
@@ -55,3 +57,24 @@ class Preprocessing():
 		for i in range(len(remove)):
 				words.remove(remove[i])
 		return words
+
+	def make_bag_of_words(self, read_words):
+		words = []
+		frequencies = []
+		
+		
+		while(len(read_words) > 0):
+			actual_word = read_words[0]
+			words.append(actual_word)
+			read_words.remove(actual_word)
+			frequencies.append(1)
+			for another_word in read_words:
+				if(another_word == actual_word):
+					frequencies[-1] += 1
+					read_words.remove(actual_word)
+		for i in range(len(words)):
+			print(words[i], frequencies[i]) 
+
+
+
+
