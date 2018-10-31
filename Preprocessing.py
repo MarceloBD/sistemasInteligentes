c1195b98c3c620d70e6677baa0b400df39a2512e
MarceloBD <marcelodianib@gmail.com>
2018-10-31 08:56:30 -0300

    reading all files

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,22 +1,34 @@
 import codecs
 import string 
+import glob
+import copy as cp
+
 class Preprocessing():
 
 	def __init__(self):
 		return
 
-	def execute(self):
-		file = codecs.open('files/CBR-837Aam274-288.txt', 'r', 'cp1251')
-		u = file.read()   # now the contents have been transformed to a Unicode string
-		out = codecs.open('processedFiles/CBR-837Aam274-288.txt', 'w', 'utf-8')
+	def execute(self, filename):
+		file = codecs.open(filename, 'r', 'cp1251')
+		#print(filename)
+		try:
+			u = file.read()   # now the contents have been transformed to a Unicode string
+		except:
+			file = codecs.open(filename, 'r', 'koi8-r')
+			u = file.read() 
+		newFilename = cp.deepcopy(filename)
+		newFilename = newFilename.split('/')[1]
+		newFilename = 'processedFiles/' + newFilename
+		print(newFilename)
+		out = codecs.open(newFilename, 'w', 'utf-8')
 		out.write(u)   # and now the contents have been output as UTF-8
 
-		with open('files/CBR-837Aam274-288-UTF8.txt') as file:
+		with open(newFilename) as file:
 		    words = [word for line in file for word in line.split()]
 		    words = self.remove_final_ponctuation(words)
 		    words = list(filter(None, words)) 
 		    words = self.delete_lower_than(words, 4)
-		    out = codecs.open('processedFiles/CBR-837Aam274-288.txt', 'w', 'utf-8')
+		    out = codecs.open(newFilename, 'w', 'utf-8')
 		    out.write('\n'.join(words))
 
 	def remove_final_ponctuation(self, words):
@@ -34,7 +46,10 @@ class Preprocessing():
 		return words
 
 	def read(self, file_name):
-		file_name = 'processedFiles/CBR-837Aam274-288.txt'
 		file = codecs.open(file_name, 'r', 'utf-8')
 		data = file.read()
 		print(data)
+
+	def convert_all_files(self):
+		filenames = glob.glob('files/*.txt')
+		[self.execute(filename) for filename in filenames]  
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from Preprocessing import Preprocessing
 
 if __name__ == '__main__':
 	prep = Preprocessing()
-	prep.execute()
+	prep.convert_all_files()
 	#prep.read('filename')
